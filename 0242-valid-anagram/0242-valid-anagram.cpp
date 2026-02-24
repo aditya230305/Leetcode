157a46020919d9bb9b0c180294e909b366f830df
aditya230305 <kumaraditya230305@gmail.com>
2026-02-24 20:27:35 +0530

    Time: 0 ms (100%), Space: 9.8 MB (47.57%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size())
+            return false;
+        unordered_map<char, int> map;
+        for (char c : s) {
+            map[c]++;
+        }
+        for (char c : t) {
+            map[c]--;
+
+            if (map[c] < 0)
+                return false;
+        }
+        return true;
+    }
+    
+};
