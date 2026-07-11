fdd23adaaee2ffff4b3c4b9a12a015a994c7341b
aditya230305 <kumaraditya230305@gmail.com>
2026-07-12 03:28:29 +0530

    Time: 0 ms (100%), Space: 71.8 MB (52.38%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,7 +4,7 @@ public:
         int sum=nums[0];
         int maxi=nums[0];
         for(int i=1;i<nums.size();i++){
-            sum=max(nums[i],nums[i]+sum);
+            sum=max(nums[i],sum+nums[i]);
             maxi=max(sum,maxi);
         }
         return maxi;
