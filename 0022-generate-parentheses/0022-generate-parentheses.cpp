f66948603e6f4e56be9d952770d5f462f75c2f96
aditya230305 <kumaraditya230305@gmail.com>
2026-08-23 00:08:26 +0530

    Time: 3 ms (68.49%), Space: 13.3 MB (76.31%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    vector<string> result;
-
+    vector<string>result;
     void solve(string &curr,int n,int open,int close){
         if(curr.length()==2*n){
             result.push_back(curr);
@@ -19,10 +18,8 @@ public:
         }
     }
     vector<string> generateParenthesis(int n) {
-        
         string curr="";
         solve(curr,n,0,0);
-
         return result;
     }
 };
